2cb470d7a03aa61cef18075ec1088d9b2e25798e
brito101 <rodrigobrito101@hotmail.com>
2022-09-04 11:21:09 -0300

    Create page course list

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,9 +1,29 @@
 <template>
-  <div class="home">Listagem dos cursos</div>
+  <div>
+    <div class="pageTitle">
+      <span class="title">Meus Cursos</span>
+      <span class="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </span>
+    </div>
+
+    <div class="content">
+      <ul class="listCourses">
+        <card-course :title="'Teste'" :image="''" :bgName="'laravel'"></card-course>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
+import CardCourse from "./components/CardCourse.vue"
+
 export default {
   name: "HomeView",
+  components: {
+    CardCourse,
+  },
 }
 </script>

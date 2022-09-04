dba352a7e0a15587f9fd83004abb80268b77bc8c
brito101 <rodrigobrito101@hotmail.com>
2022-09-04 10:57:26 -0300

    Create Template

---
+++ src/layouts/components/FooterComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer>
+    <span class="copyright fontSmall">
+      Todos os Direitos reservados - <b>Estágio Premium</b>
+    </span>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "footer-component",
+}
+</script>
---
+++ src/views/supports/MySupports.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="home">Minhas dúvidas</div>
+</template>
+
+<script>
+export default {
+  name: "MySupports",
+}
+</script>
